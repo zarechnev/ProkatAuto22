6b01584f7c2f40e6dbc838edf481dc3e4d96fcd3
Jensen-kaz <kazuhirajob@gmail.com>
2017-11-07 13:31:00 +0700

    Правка методов формы, удалил несуществующие методы(delete), добавил методы в классы ReadAllCustumers\Drivers в классы

--- GUI/GUI/Classes/DataBaseClass.cs
+++ GUI/GUI/Classes/DataBaseClass.cs
@@ -190,14 +190,35 @@ namespace ProkatAuto22.Classes
             return (ListOfDrivers);
         }
 
+
+
+
+        ///Записывает в базу нового клиента. Не протестирован.
         public void AddNewCustomerDB(CustomerClass NewCostomer)
         {
 
         }
 
-        public void ReadCustomerDB(CustomerClass ReadCustomer)
+        ///Считывает из базы нового клиента. Не протестирован.
+        public void ReadCustomerDB(int CustomerID)
         {
 
         }
+
+
+        ///Редактирует в базе нового клиента. Не протестирован.
+        public void EditCustomerDB(CustomerClass EditCustomer)
+        {
+
+        }
+
+        ///Возвращает список клиентов базы. Не протестирован.
+        public List<CustomerClass> ReadAllCustomersDB()
+        {
+            List<CustomerClass> ListOfCustomers = new List<CustomerClass>();
+
+            return (ListOfCustomers);
+        }
+
     }
 }
