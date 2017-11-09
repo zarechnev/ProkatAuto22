a0e1b41c4f32757e5d2c9fa5f2618beaf1fb1fa3
Jensen-kaz <kazuhirajob@gmail.com>
2017-11-09 16:07:45 +0700

    Доработал методы клиентов

--- GUI/GUI/Classes/OrderClass.cs
+++ GUI/GUI/Classes/OrderClass.cs
@@ -8,27 +8,70 @@ namespace ProkatAuto22.Classes
 {
     class OrderClass
     {
-        public string NumberRequest { get; set; }      
-        public string DriverCar { get; set; }
-        public string DurationLease { get; set; }
-        public string PriceLease { get; set; }
-        public string KidsChair { get; set; }
-        public string WinterTires { get; set; }
-        public string SportFastenings { get; set; }
-        public string Gps { get; set; }
+        public string IDRequest { get; set; }
+        public string DataRequest { get; set; }
+        public string CarRequest { get; set; }
+        public string DriverRequest { get; set; }
+        public string CustomerRequest { get; set; }
+        public string AddressRequest { get; set; }
+        public string TimeRequest { get; set; }
+        public string PriceRequest { get; set; }
+        public bool KidsChair { get; set; }
+        public bool WinterTires { get; set; }
+        public bool SportFastenings { get; set; }
+        public bool Gps { get; set; }
 
-        public void InsertRequest()
+        private DataBaseClass DB;
+
+        /// <summary>
+        /// Метод класса. Возвращает список (классов) заявок.
+        /// </summary>
+        /// <returns></returns>
+        public static List<OrderClass> ReadAllOrders()
         {
-
+            DataBaseClass DB = new DataBaseClass();
+            return DB.ReadAllOrdersDB();
         }
 
-        public void ReadRequest()
+        /// <summary>
+        /// Конструктор класса.
+        /// </summary>
+        public OrderClass()
         {
+            DB = new DataBaseClass();
         }
 
-        public void DeleteRequest()
+        /// <summary>
+        /// Добавление заявки.
+        /// </summary>
+        public void InsertOrder()
         {
+            DB.AddNewOrderDB(this);
+        }
 
+        /// <summary>
+        /// Редактирование заявки.
+        /// </summary>
+        public void EditOrder()
+        {
+            DB.EditOrderDB(this);
+        }
+
+        /// <summary>
+        /// Удаление заявки.
+        /// </summary>
+        public void DeleteOrder()
+        {
+            DB.DeleteOrderDB(this);
+        }
+
+        /// <summary>
+        /// Выводит информацию о экземпляре в читаемом виде.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.IDRequest + ", " + this.CarRequest + ", клиент: " + this.CustomerRequest;
         }
     }
 }
